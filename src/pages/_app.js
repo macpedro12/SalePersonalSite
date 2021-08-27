e1686e8a556ac1b6fd3869521c9ef328f4d70b1f
macpedro12 <pedroamorim62@gmail.com>
2021-08-27 12:46:59 -0300

    added aos

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/global.scss'
-
+import "../styles/global.scss";
+import AOS from "aos";
+import { useEffect } from "react";
+import "aos/dist/aos.css";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    AOS.init({
+      easing: "ease-out-cubic",
+      offset: 50,
+      mirror:true, 
+    });
+  }, []);
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
